4f1a1be97478a33e2f2d758725e95aa868fb2aa9
Gardier S <sebastiengardier@outlook.com>
2022-10-01 19:24:18 +0200

    Routing Tests

---
+++ primeflix/src/components/Compteur.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="compteur">
+        <h1>
+            Compteur
+        </h1>
+        <p>
+            Vous avez cliqué {{ total }} fois.
+        </p>
+        <button @click="increment">
+            cliquez ici !
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Compteur',
+        data(){
+            return{
+                total: 0
+            }
+        },
+        methods: {
+            increment(){
+                this.total += 1;
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ primeflix/src/views/Browse.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="browse">
+      <h1>This is the {{ $route.params.format }} page browser</h1>
+      <Compteur/>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: 'Contact'
+    }
+  </script>
